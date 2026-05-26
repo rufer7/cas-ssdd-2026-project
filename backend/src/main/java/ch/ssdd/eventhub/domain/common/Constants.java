54723e776ea623d48952ec9288a2892760a3e8c7
Marc Rufer <marc.rufer@isolutions.ch>
2026-05-26 21:54:08 +0200

    Fix SonarQube Cloud findings

--- backend/src/main/java/ch/ssdd/eventhub/domain/common/Constants.java
+++ backend/src/main/java/ch/ssdd/eventhub/domain/common/Constants.java
@@ -1,5 +1,10 @@
 package ch.ssdd.eventhub.domain.common;
 
 public class Constants {
+
+    private Constants() {
+        // Private constructor to prevent instantiation
+    }
+
     public static final int DEFAULT_MAX_STRING_LENGTH = 255;
 }
