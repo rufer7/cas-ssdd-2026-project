dc4f0f3a0e81e8ad512a7600e0c52788419482a5
Marc Rufer <marc.rufer@isolutions.ch>
2026-06-02 00:01:55 +0200

    minor

--- backend/src/main/java/ch/ssdd/eventhub/ports/inbound/CreateEventUseCase.java
+++ backend/src/main/java/ch/ssdd/eventhub/ports/inbound/CreateEventUseCase.java
@@ -1,7 +1,6 @@
 package ch.ssdd.eventhub.ports.inbound;
 
 import ch.ssdd.eventhub.domain.Event;
-import ch.ssdd.eventhub.domain.User;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +12,5 @@ public interface CreateEventUseCase {
             LocalDateTime from,
             LocalDateTime to,
             String location,
-            String username
-    );
+            String username);
 }
