171c0e90c1cc706a04207572be18508ad877a30e
Kevin <k4ito@outlook.com>
2026-06-07 20:17:30 +0200

    Update backend/src/main/java/ch/ssdd/eventhub/domain/command/CreateEventCommand.java
    Co-authored-by: Marc Rufer <rufer7@users.noreply.github.com>

--- backend/src/main/java/ch/ssdd/eventhub/domain/command/CreateEventCommand.java
+++ backend/src/main/java/ch/ssdd/eventhub/domain/command/CreateEventCommand.java
@@ -16,7 +16,7 @@ public record CreateEventCommand(
         Objects.requireNonNull(from, "Start date is required");
         Objects.requireNonNull(to, "End date is required");
         Objects.requireNonNull(location, "End date is required");
-        Objects.requireNonNull(username, "End date is required");
+        Objects.requireNonNull(username, "Username is required");
         if (to.isBefore(from)) {
             throw new IllegalArgumentException("End date cannot be before start date");
         }
