5b21a9e277e7ed63755f6e8bac0af6adec0310df
Marc Rufer <marc.rufer@isolutions.ch>
2026-06-08 22:25:24 +0200

    Another try

--- backend/src/main/java/ch/ssdd/eventhub/adapters/outbound/persistence/NoteRepository.java
+++ backend/src/main/java/ch/ssdd/eventhub/adapters/outbound/persistence/NoteRepository.java
@@ -2,7 +2,9 @@ package ch.ssdd.eventhub.adapters.outbound.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NoteRepository extends JpaRepository<NoteEntity, UUID> {
+    List<NoteEntity> findByCreatedBy(UserEntity user);
 }
