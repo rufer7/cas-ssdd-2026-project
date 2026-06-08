6a611bfe8a62455bc382189ef32e4a25836a07f6
Marc Rufer <marc.rufer@isolutions.ch>
2026-06-08 22:17:11 +0200

    Fix build

--- backend/src/main/java/ch/ssdd/eventhub/adapters/inbound/rest/NoteRestController.java
+++ backend/src/main/java/ch/ssdd/eventhub/adapters/inbound/rest/NoteRestController.java
@@ -8,6 +8,7 @@ import org.owasp.html.PolicyFactory;
 import org.owasp.html.Sanitizers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
