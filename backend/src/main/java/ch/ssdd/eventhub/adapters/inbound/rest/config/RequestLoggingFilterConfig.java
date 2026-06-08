756104f34031178402ef0399ef53f7989f89063b
Marc Rufer <marc.rufer@isolutions.ch>
2026-06-09 00:21:00 +0200

    Exclude headers from logging

--- backend/src/main/java/ch/ssdd/eventhub/adapters/inbound/rest/config/RequestLoggingFilterConfig.java
+++ backend/src/main/java/ch/ssdd/eventhub/adapters/inbound/rest/config/RequestLoggingFilterConfig.java
@@ -13,7 +13,7 @@ public class RequestLoggingFilterConfig {
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
         filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(true);
+        filter.setIncludeHeaders(false);
         filter.setAfterMessagePrefix("REQUEST DATA: ");
 
         return filter;
