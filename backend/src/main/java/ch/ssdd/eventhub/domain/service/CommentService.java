0b3599d2c1412389458c0c61acb3fe9c6fb9d810
Marc Rufer <rufer7@users.noreply.github.com>
2026-06-09 13:16:41 +0200

    Apply suggestion from @rufer7

--- backend/src/main/java/ch/ssdd/eventhub/domain/service/CommentService.java
+++ backend/src/main/java/ch/ssdd/eventhub/domain/service/CommentService.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
