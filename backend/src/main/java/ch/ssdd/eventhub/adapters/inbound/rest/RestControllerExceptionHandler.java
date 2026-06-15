ee0541e58b180ec36abde3e77da7b406a32ad9bb
Marc Rufer <marc.rufer@isolutions.ch>
2026-06-15 13:04:17 +0200

    mimimi

--- backend/src/main/java/ch/ssdd/eventhub/adapters/inbound/rest/RestControllerExceptionHandler.java
+++ backend/src/main/java/ch/ssdd/eventhub/adapters/inbound/rest/RestControllerExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestControllerExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(RestControllerExceptionHandler.class);
-    
+
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleException(IllegalArgumentException ex) {
